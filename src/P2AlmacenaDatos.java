be750c7accac4939443b3653a99e0b31d4dd8f04
Jose Manuel Cózar <jcozmar0708@g.educaand.es>
2024-03-19 11:13:54 +0100

    Final examen

--- src/P2AlmacenaDatos.java
+++ src/P2AlmacenaDatos.java
@@ -1,52 +1,28 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class P2AlmacenaDatos {
-    private static int TAMANYO_CABECERA = 4;
-    private static int TAMANYO_REGISTRO = TAMANYO_CABECERA + 8;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String linea;
-        double marca;
-        int numRegistros = 0;
-        do {
-            linea = sc.nextLine();
-            linea = linea.replace(" ", "");
-            int dia = Integer.parseInt(linea.substring(0, 1));
-            if (dia != 0) {
-                long registro_actual = 0;
-                try {
-                    if (dia < 0 || dia > 5) System.out.println("Día incorrecto");
-                    else {
-                        marca = Double.parseDouble(linea.substring(1));
-                        File fichero = new File("marcas.dat");
-                        long posicion;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                String linea = br.readLine();
+                if (linea.equals("0")) break;
+                String[] split = linea.split(" ");
+                int dia = Integer.parseInt(split[0]);
+                if (dia < 0 || dia > 5) System.out.println("Día incorrecto");
+                else {
+                    double marca = Double.parseDouble(split[1]);
+                    File fichero = new File("marcas.dat");
+                    try (RandomAccessFile raf = new RandomAccessFile(fichero, "rw")) {
                         if (!fichero.exists()) {
-                            try (RandomAccessFile raf = new RandomAccessFile(fichero, "rw")) {
-                                raf.seek(0);
-                                raf.writeInt(numRegistros + 1);
-                                raf.writeInt(dia);
-                                raf.writeDouble(marca);
-                            } catch (IOException e) {
-                                System.out.println("Ha ocurrido un error de entrada / salida");
+                            for (int i = 1; i <= 5; i++) {
+                                raf.writeDouble(Double.NaN);
                             }
                         }
-                        try (RandomAccessFile raf = new RandomAccessFile(fichero, "rw")) {
-                            posicion = TAMANYO_CABECERA + TAMANYO_REGISTRO * registro_actual;
-                            raf.setLength(posicion + TAMANYO_REGISTRO);
-                            raf.seek(posicion);
-                            raf.writeInt(numRegistros + 1);
-                            raf.writeInt(dia);
-                            raf.writeDouble(marca);
-                        } catch (IOException e) {
-                            System.out.println("Ha ocurrido un error de entrada / salida");
-                        }
-                        registro_actual++;
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println("Falta escribir la marca del día");
                 }
             }
-        } while (linea.charAt(0) != '0');
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error de entrada/salida");
+        }
     }
 }
